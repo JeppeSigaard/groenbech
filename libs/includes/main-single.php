297a881c6e0db74990393f2b57fccdd61ee8c865
JeppeSigaard <jeppe@smatmonkey.dk>
2015-05-29 10:17:06 +0200

    hardcode ændring af keyword tekst, single

--- libs/includes/main-single.php
+++ libs/includes/main-single.php
@@ -28,7 +28,7 @@
     <?php  $i = 0;$tags = wp_get_post_terms(get_the_ID(),'emne');?>
     <?php if(!empty($tags)): ?>
     <ul class="tags-list">
-        <strong><?php echo smamo_lang('Nøgleord:','Keywords:');?></strong>
+        <strong><?php echo smamo_lang('Læs mere om: ','More about: ');?></strong>
         <?php foreach($tags as $tag): $i++; $name = smamo_lang($tag->name,get_tax_meta($tag->term_id,'emne_engelsk')); if ($name !== '') :
         $link = get_bloginfo('url').'/'.$tag->taxonomy.'/'.$tag->slug;?>
         <li>

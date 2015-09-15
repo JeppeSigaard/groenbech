19312eafd22efa45542275953c99bdbeb42c6d90
JeppeSigaard <jeppe@smatmonkey.dk>
2015-09-15 10:06:27 +0200

    oprettet nyheder

--- libs/includes/sidebar-single.php
+++ libs/includes/sidebar-single.php
@@ -1,10 +1,25 @@
-<?php ?>
+<?php 
+
+$related = new WP_Query(array(
+    'post_type' => 'post',
+    'posts_per_page'    => 3,
+    'post__not_in' => array(get_the_ID()),
+    'orderby'   => 'post_date',
+    'order' => 'DESC',
+));
+
+if($related->have_posts()) :
+
+?>
 <ul class="post-list">
+   <?php while ($related->have_posts()) : $related->the_post(); ?>
     <li class="<?php post_class('post-list-item'); ?>" role="article">
         <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+            <?php if(has_post_thumbnail()) :?>
             <div class="post-img">
-                <img src="http://192.168.1.4/groenbech/wp-content/uploads/2015/08/DRA-Case-560x315.png" class="attachment-postlist wp-post-image" alt="Annoncer">
+                <?php the_post_thumbnail('postlist') ?>
             </div>
+            <?php endif; ?>
             <div class="post-text">
                 <h6 class="post-title"><?php the_title(); ?></h6>
                 <div class="post-desc">
@@ -16,7 +31,10 @@
             <a href="<?php the_permalink(); ?>" title="Læs mere"> Læs mere </a>
         </div>
     </li>
+    <?php endwhile; ?>
 </ul>
+
+<?php endif; ?>
 
 
 
---
+++ page-archive.php
@@ -0,0 +1,24 @@
+<?php
+/*
+template name: Nyhedsarkiv
+*/
+get_header(); ?>
+<section id="content">
+    <main role="main" class="main-full">
+       <h1 class="single-title"><?php echo smamo_lang('Nyheder','News') ?></h1>
+        <?php $postlist_query = array(
+            'posts_per_page'   => -1,
+            'offset'           => 0,
+            'orderby'          => 'post_date',
+            'order'            => 'DESC',
+            'post_type'        => 'post',
+            'post_status'      => 'publish',
+            'suppress_filters' => true,
+            'num_words'        => 20,
+        ); ?>
+        <div class="bottom">
+            <?php include 'libs/includes/post-list.php'; ?>
+        </div>
+    </main>
+</section>
+<?php get_footer(); ?>
